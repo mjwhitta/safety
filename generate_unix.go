54083e31fc2e6c139d79fbf7458316d558cd836f
Miles Whittaker <mj@whitta.dev>
2021-12-05 12:27:48 -0500

    Moved generate stuff to unix only

---
+++ generate_unix.go
@@ -0,0 +1,6 @@
+//go:build !windows
+// +build !windows
+
+package safety
+
+//go:generate ./scripts/generate_go_funcs
--- globals.go
+++ globals.go
@@ -1,6 +1,4 @@
 package safety
 
-//go:generate ./scripts/generate_go_funcs
-
 // Version is the package version.
 const Version = "1.11.0"
